24c988c1d5f1a8458fddbe6f344c309f6f883a74
ophl55 <oliver.philipps@mni.thm.de>
2016-06-01 10:54:29 +0200

    updates client

--- src/t3/sos/fi/upm/es/client/UserManagementWSClient.java
+++ src/t3/sos/fi/upm/es/client/UserManagementWSClient.java
@@ -6,14 +6,28 @@ import t3.sos.fi.upm.es.client.UserManagementWSStub.*;
 
 public class UserManagementWSClient {
 	public static void main(String args[]) throws RemoteException{
+		Response response;
+		
 		UserManagementWSStub stub = new UserManagementWSStub();
 		stub._getServiceClient().engageModule("addressing");
 		stub._getServiceClient().getOptions().setManageSession(true);
-
+;
+		
 		User user = new User();
 		user.setName("admin");
 		user.setPwd("admin");
-		Response response = stub.login(user);
+
+		stub._getServiceClient().getServiceContext().setProperty("USER", user);
+		
+		response = stub.login(user);
+		
 		System.out.println("Result login: " + response.getResponse());
+		System.out.println(stub._getServiceClient().getServiceContext().getGroupName());
+		
+
+		response = stub.login(user);
+		System.out.println("Result login: " + response.getResponse());
+		System.out.println(stub._getServiceClient().getServiceContext().getGroupName());
+		
 	}
 }
