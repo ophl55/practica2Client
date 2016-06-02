bd0f502d3ecf48d8eaeff8ffcf752046638a21cf
ophl55 <oliver.philipps@mni.thm.de>
2016-06-02 17:34:43 +0200

    adds some tests to the client

--- src/t3/sos/fi/upm/es/client/UserManagementWSClient.java
+++ src/t3/sos/fi/upm/es/client/UserManagementWSClient.java
@@ -10,8 +10,11 @@ import t3.sos.fi.upm.es.client.UserManagementWSStub.Username;
 public class UserManagementWSClient {
 	public static void main(String args[]) throws RemoteException {
 		Response response;
-
 		UserManagementWSStub stub = new UserManagementWSStub();
+
+		// ----------------------------------------------------------//
+		// CREATE A 1ST SESSION //
+		// ----------------------------------------------------------//
 		stub._getServiceClient().engageModule("addressing");
 		stub._getServiceClient().getOptions().setManageSession(true);
 
@@ -21,11 +24,11 @@ public class UserManagementWSClient {
 		user.setPwd("admin");
 
 		response = stub.login(user);
-
-		System.out.println("Result login: " + response.getResponse());
+		System.out.println("Login as superuser: " + response.getResponse());
 
 		response = stub.login(user);
-		System.out.println("Result login: " + response.getResponse());
+		System.out.println("Login a 2nd time as superuser: "
+				+ response.getResponse());
 		System.out.println();
 
 		// superuser adds user
@@ -57,8 +60,7 @@ public class UserManagementWSClient {
 		response = stub.removeUser(username);
 		System.out
 				.println("Superuser trying to remove user that doesn't exist: "
-						+ response);
-		System.out.println();
+						+ response.getResponse());
 
 		// superuser removes existing user "testuser" (added before)
 		username = new Username();
@@ -66,31 +68,50 @@ public class UserManagementWSClient {
 
 		response = stub.removeUser(username);
 		System.out.println("Superuser trying to remove user that exists: "
-				+ response);
+				+ response.getResponse());
+		response = stub.addUser(userToAdd);
+		System.out.println("Adding a non existing user again (as superuser): "
+				+ response.getResponse());
 		System.out.println();
 
 		// logout superuser
 		stub.logout();
 
+		// ----------------------------------------------------------//
+		// CREATE A 2ND SESSION //
+		// ----------------------------------------------------------//
 		stub = new UserManagementWSStub();
 		stub._getServiceClient().engageModule("addressing");
 		stub._getServiceClient().getOptions().setManageSession(true);
-
-		// not logged in user tries to change his password
-		pwPair = new PasswordPair();
-		pwPair.setOldpwd("pw1");
-		pwPair.setNewpwd("pw2");
-		response = stub.changePassword(pwPair);
-		System.out.println("Not logged in user trying to change his password: "
-				+ response.getResponse());
-
-		// user changes password (with incorrect oldPassword)
+		
+		// Login of non registered user
 		User user1 = new User();
 		user1.setName("user1");
 		user1.setPwd("user1");
+		response = stub.login(user1);
+		System.out.println("Login of not registered User: " + response.getResponse());
+		System.out.println();
+		
+		// ----------------------------------------------------------//
+		// CREATE A 3RD SESSION //
+		// ----------------------------------------------------------//
+		stub = new UserManagementWSStub();
+		stub._getServiceClient().engageModule("addressing");
+		stub._getServiceClient().getOptions().setManageSession(true);
+		
+		// not logged in user tries to change his password
+		pwPair = new PasswordPair();
+		pwPair.setOldpwd("testuser");
+		pwPair.setNewpwd("testuserNewpw");
+		response = stub.changePassword(pwPair);
+		System.out.println("Not logged in user trying to change his password: "
+				+ response.getResponse());
+		
+		// Testuser logs in
+		response = stub.login(userToAdd);
+		System.out.println("User \"testuser\" trys to login: " + response.getResponse());
 
-		stub.login(user1);
-
+		// user changes password (with incorrect oldPassword)
 		pwPair = new PasswordPair();
 		pwPair.setOldpwd("wrongOldPassword");
 		pwPair.setNewpwd("pw");
@@ -100,11 +121,11 @@ public class UserManagementWSClient {
 						+ response.getResponse());
 
 		// normal user calls addUser/removeUser
-		response = stub.addUser(new User());
-		System.out.println("Adding new user withoud permission: "
+		response = stub.addUser(user);
+		System.out.println("Adding new user without permission: "
 				+ response.getResponse());
-		response = stub.removeUser(new Username());
-		System.out.println("Removing user withoud permission: "
+		response = stub.removeUser(username);
+		System.out.println("Removing user without permission: "
 				+ response.getResponse());
 		System.out.println();
 
